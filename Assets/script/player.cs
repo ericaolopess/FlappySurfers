1f7f21ebcc1105c57ee659c2965fbd779bbb998d
GrupoSequencial <33881813+ericaolopess@users.noreply.github.com>
2022-05-31 21:53:06 -0300

    plataforma infinita

--- Assets/script/player.cs
+++ Assets/script/player.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class player : MonoBehaviour
+public class Player : MonoBehaviour
 {
     private CharacterController controller;
     private bool start;
+    private Animator anin;
+
+    public int contador;
+    public Text pontos;
 
     public float speed;
     public float jump;
@@ -14,13 +19,25 @@ public class player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        contador = 0;
+        pontos.text = "" + contador;
+
         controller = GetComponent<CharacterController>();
+        anin = GetComponent<Animator>();
         start = true;
+
+        anin.SetBool("idle", true);
+        anin.SetBool("run", false);
+        anin.SetBool("jump", false);
+        anin.SetBool("slide",false);
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
+
         if (start)
         {
             if (Input.GetKeyDown(KeyCode.Space))
@@ -28,13 +45,44 @@ public class player : MonoBehaviour
                 Vector3 direction = Vector3.forward * speed;
                 controller.Move(direction * Time.deltaTime);
                 start = false;
+
+                contador += 100;
+                pontos.text = "" + contador;
+
+                anin.SetBool("idle", false);
+                anin.SetBool("run", true);
+                anin.SetBool("jump", false);
+                anin.SetBool("slide", false);
             }
         }
-        if (!start)
+        else
         {
             Vector3 direction = Vector3.forward * speed;
             controller.Move(direction * Time.deltaTime);
+            contador += 100;
+            pontos.text = "" + contador;
+        }
+
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            float x = Input.GetAxis("Horizontal");
+            Vector3 move = transform.right * x;
+            controller.Move(move * speed * Time.deltaTime);
+            anin.SetBool("idle", false);
+            anin.SetBool("run", true);
+            anin.SetBool("jump", false);
+            anin.SetBool("slide", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            anin.SetBool("idle", false);
+            anin.SetBool("run", false);
+            anin.SetBool("jump", true);
+            anin.SetBool("slide", false);
+
         }
 
     }
 }
+
--- Assets/script/spawnPlataforma.cs
+++ Assets/script/spawnPlataforma.cs
@@ -2,23 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class spawnPlataforma : MonoBehaviour
+public class SpawnPlataforma : MonoBehaviour
 {
-
     public List<GameObject> plataformas = new List<GameObject>();
+    public int distancia;
 
     // Start is called before the first frame update
     void Start()
     {
-        for(int i = 0; i < plataformas.Count; i++)
+
+        for (int i = 0; i < plataformas.Count; i++)
         {
-            Instantiate(plataformas[i], new Vector3(0, 0, i * 86), transform.rotation);
+           Instantiate(plataformas[i], new Vector3(0,0, i * 86), transform.rotation);
+            distancia += 86;
         }
+
+        InvokeRepeating("Recicle", 15f, 30f);
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Recicle()
     {
-        
+
+        for (int i = 0; i < plataformas.Count; i++)
+        { 
+            Instantiate(plataformas[i], new Vector3(0, 0, distancia), transform.rotation);
+            plataformas[i].transform.position = new Vector3(0, 0, distancia);
+            distancia += 86;
+        }
+
     }
+
+
 }
